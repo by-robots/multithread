7ed3fbcbcc85511edbc048fad167bc3db9908d2a
Dan Gates <dan.gates@pentangle.co.uk>
2017-09-10 12:27:40 +0100

    Get tests working.

---
+++ src/ManyPies.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ByRobots\ManyPies;
+
+class ManyPies
+{
+    //
+}
---
+++ tests/AddingTasks/AddOneTask.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ByRobots\ManyPies\Tests\AddingTasks;
+
+use ByRobots\ManyPies\Tests\TestCase;
+
+class AddOneTask extends TestCase
+{
+    //
+}
