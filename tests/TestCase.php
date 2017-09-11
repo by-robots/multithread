acb2b52ab45ea12f8de2e485384f615e0fddb91e
Dan Gates <dan.gates@pentangle.co.uk>
2017-09-11 21:52:29 +0100

    Fix processes all outputting.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,6 +12,10 @@ abstract class TestCase extends MockeryTestCase
         if (function_exists('\\ByRobots\\Multithread\\Runner\\pcntl_fork')) {
             require_once __DIR__ . '/PCNTL.php';
         }
+
+        if (!defined('IN_PHPUNIT')) {
+            define('IN_PHPUNIT', true);
+        }
     }
 
     public function tearDown()
